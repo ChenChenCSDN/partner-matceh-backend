3b718efa2d0eeb67fabe303b19dedd761cc75c6b
ChenSir <1652848896@qq.com>
2024-06-12 19:25:09 +0800

    用户中心初始架构

---
+++ src/main/java/com/java/usercenter/mapper/UserMapper.java
@@ -0,0 +1,8 @@
+package com.java.usercenter.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.java.usercenter.model.User;
+
+public interface UserMapper extends BaseMapper<User> {
+
+}
